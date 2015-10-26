137a230108335a258153295b4268bc526a115d82
Michael Esposito <michaelde1984@gmail.com>
2015-10-26 11:12:35 -0400

    removed password validation

--- app/components/signup.js
+++ app/components/signup.js
@@ -25,7 +25,6 @@ render () {
       <input type="text" placeholder="Email" defaultValue={this.props.email} ref="email" />
       <input type="text" placeholder="User Name" defaultValue={this.props.userName} ref="username" />
       <input type="text" placeholder="password" defaultValue={this.props.password} ref="password" />
-      <input type="text" placeholder="validate passsword" defaultValue={this.props.password} ref="password" />
       <button>Submit</button>
     </form>
     );
