6da98ad4231ac1fbd4ba37dd7c9650840a33596d
Anthony Bluemmel <tonyxblues@me.com>
2015-10-27 11:06:44 -0400

    added first and last name to the PropTypes and added first and last name inputs

--- app/components/signupform.js
+++ app/components/signupform.js
@@ -5,6 +5,8 @@ import store from '../store';
 
 const Signup = React.createClass({
   propTypes: {
+    firstname: React.PropTypes.string,
+    lastname: React.PropTypes.string,
     email: React.PropTypes.string,
     username: React.PropTypes.string,
     password: React.PropTypes.string
@@ -13,6 +15,8 @@ const Signup = React.createClass({
   handleSubmit(e) {
   e.preventDefault();
   store.getUserCollection().create({
+    firstname: this.refs.firstname.value,
+    lastname: this.refs.lastname.value,
     email: this.refs.email.value,
     username: this.refs.username.value,
     password: this.refs.password.value,
@@ -24,6 +28,9 @@ render () {
   return (
     <div className="signup-container">
       <form action="">
+        <button className="signup-back"><i className="fa fa-chevron-left"></i></button>
+        <input className="signup-first" type="text" placeholder="First Name" defaultValue={this.props.firstname} ref="firstname" />
+        <input className="signup-last" type="text" placeholder="Last Name" defaultValue={this.props.lastname} ref="lastname" />
         <input className="signup-email" type="text" placeholder="Email" defaultValue={this.props.email} ref="email" />
         <input className="signup-username" type="text" placeholder="Username" defaultValue={this.props.username} ref="username" />
         <input className="signup-password" type="text" placeholder="Password" defaultValue={this.props.password} ref="password" />
