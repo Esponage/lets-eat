12894819236b70198309be3c8a6707efb882e489
Michael Esposito <michaelde1984@gmail.com>
2015-11-06 09:56:57 -0500

    working on functionality for next/prev page

--- app/models/restaurant-collection.js
+++ app/models/restaurant-collection.js
@@ -4,14 +4,23 @@ import $ from 'jquery';
 import '../jsonpproxy.js';
 
 var RestaurantCollection = Backbone.Collection.extend({
+  page: 0,
  url() {
    // return "https://jsonp.afeld.me/?url=" + encodeURIComponent("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + this.search + "&key=AIzaSyC-Q2PvTSqrIoZpPWF9rlTcDoIJtxVZQcM&libraries=places");
-     return "http://localhost:5050/api/v2.1/search?q=" + this.search;
+     return "http://localhost:5050/api/v2.1/search?q=" + this.search + "&start=" + this.page;
  },
 
  setSearch(search) {
    this.search = search;
  },
+
+ nextPage() {
+   this.page = this.page + 19;
+ },
+
+ prevPage() {
+  this.page = this.page - 19;
+},
 
  parse(response) {
    console.log(response);
